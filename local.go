2a0d0f410e4005e75ca9eabff8efbee8337fd585
Harry Wolff <hswolff@gmail.com>
2024-05-13 15:24:49 -0400

    small refactor

--- local.go
+++ local.go
@@ -24,8 +24,8 @@ func calculateLocalDeltas(console string, romDir string) {
 
 	filesExtensions := path.Ext(romFiles[0].Name())
 	romFileCache := []RomFile{}
-	var missingRemotes []string
 	possibleMismatches := []RomFile{}
+	missingRemotes := []RomFile{}
 
 	remoteRomFiles, allRemoteRomNames := getRemoteRomFiles(console)
 
@@ -36,22 +36,23 @@ func calculateLocalDeltas(console string, romDir string) {
 		sort.Sort(results)
 		// fmt.Printf("Searching for \"%s\"\n", fileNameWithoutExtension)
 
+		romFile := RomFile{
+			LocalName: fileName,
+		}
 		if results.Len() > 0 {
 			remoteRomFile := remoteRomFiles[results[0].Target]
-			romFile := RomFile{
-				LocalName:  fileName,
-				RemoteRom:  remoteRomFile,
-				AllMatches: results,
-			}
+			romFile.RemoteRom = remoteRomFile
+			romFile.AllMatches = results
+
 			romFileCache = append(romFileCache, romFile)
 
 			if results[0].Distance > 20 {
 				possibleMismatches = append(possibleMismatches, romFile)
 			}
 		} else {
-			missingRemotes = append(missingRemotes, fileName)
+			missingRemotes = append(missingRemotes, romFile)
 		}
 	}
 
-	PrettyPrint(possibleMismatches)
+	PrettyPrint(missingRemotes)
 }
