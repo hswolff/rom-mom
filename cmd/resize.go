ca96e706ac3e0abe27c8daef2b6da871c1f375c8
Harry Wolff <hswolff@gmail.com>
2024-05-17 22:07:23 -0400

    implement resize

--- cmd/resize.go
+++ cmd/resize.go
@@ -2,19 +2,87 @@ package cmd
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"path/filepath"
 
+	"github.com/anthonynsimon/bild/imgio"
+	"github.com/anthonynsimon/bild/transform"
+	"github.com/hswolff/rom-mom/lib"
 	"github.com/spf13/cobra"
+)
+
+var (
+	width  int
+	height int
 )
 
 func init() {
 	rootCmd.AddCommand(resizeCmd)
+
+	resizeCmd.Flags().IntVar(&width, "width", 640, "width to resize images to")
+	resizeCmd.Flags().IntVar(&height, "height", 480, "height to resize images to")
 }
 
+// 640x480 image size
 var resizeCmd = &cobra.Command{
 	Use:   "resize",
 	Short: "resize images to desired dimension",
 	Long:  "resize images to desired dimension",
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("DOWNLOAD")
+	RunE: func(cmd *cobra.Command, args []string) error {
+		fmt.Printf("\n~~resize images~~\n")
+		romFiles, _, _ := lib.CalculateLocalDeltas(console, romDir)
+
+		fmt.Printf("Local ROM files found:\t\t\t %v\n", len(romFiles))
+		fmt.Printf("Number of matching ROMs:\t\t %v\n", len(romFiles.Matches()))
+
+		if dryRun {
+			fmt.Printf("\n~~DRY RUN - NOTHING BEING RESIZED~~\n")
+		}
+
+		imageFolderPath := filepath.Join(romDir, "images", console)
+		images, err := os.ReadDir(imageFolderPath)
+		if err != nil {
+			return err
+		}
+
+		for _, image := range images {
+			imagePath := filepath.Join(imageFolderPath, image.Name())
+			img, err := imgio.Open(imagePath)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			bounds := img.Bounds()
+			currentWidth, currentheight := bounds.Dx(), bounds.Dy()
+
+			alreadyResized := false
+			if width == currentWidth && height == currentheight {
+				alreadyResized = true
+			}
+
+			action := "RESIZING"
+			if alreadyResized {
+				action = "SKIPING, ALREADY RESIZED"
+			}
+
+			fmt.Printf("\n%s  (%s)\n", imagePath, action)
+
+			if !dryRun {
+				if alreadyResized {
+					continue
+				}
+
+				resized := transform.Resize(img, width, height, transform.Linear)
+				if err := imgio.Save(imagePath, resized, imgio.PNGEncoder()); err != nil {
+					fmt.Printf("\tResized: Error \t%s\n", err)
+				} else {
+					fmt.Printf("\tResized: Success\n")
+				}
+			}
+
+		}
+
+		return nil
 	},
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.22.3
 
 require (
 	github.com/PuerkitoBio/goquery v1.9.2
+	github.com/anthonynsimon/bild v0.13.0
 	github.com/lithammer/fuzzysearch v1.1.8
 	github.com/spf13/cobra v1.8.0
 )
@@ -12,6 +13,7 @@ require (
 	github.com/andybalholm/cascadia v1.3.2 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/image v0.0.0-20190703141733-d6a02ce849c9 // indirect
 	golang.org/x/net v0.24.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 )
